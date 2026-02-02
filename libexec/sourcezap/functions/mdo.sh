6a8972c7daf8295101bf922aac4907e03d65fb76
0x1eef <0x1eef@protonmail.com>
2026-02-01 21:57:27 -0300

    Move *_rule functions to mdo.sh

--- libexec/sourcezap/functions/mdo.sh
+++ libexec/sourcezap/functions/mdo.sh
@@ -6,6 +6,39 @@ get_system_rules()
     sysctl -n security.mac.do.rules 2> /dev/null || true
 }
 
+merge_rules()
+{
+    local lh rh
+    lh=$1
+    rh=$2
+    printf "%s\n%s\n" "${lh}" "${rh}" | \
+        tr ';' '\n' | \
+        awk 'NF' | \
+        awk '!seen[$0]++' | \
+        paste -sd ';' -
+}
+
+filter_rules()
+{
+    local system_rules sourcezap_rules
+    system_rules=$1
+    sourcezap_rules=$2
+    printf "%s" "${system_rules}" | tr ';' '\n' | awk -v rulez="${sourcezap_rules}" '
+BEGIN { n = split(rulez, r, "\n") }
+NF {
+    for (i = 1; i <= n; i++) if ($0 == r[i]) next
+    print
+}
+'
+}
+
+join_rules()
+{
+    local rule_list
+    rule_list=$1
+    printf "%s" "${rule_list}" | paste -sd ';' -
+}
+
 get_sourcezap_rules()
 {
     local libexec
