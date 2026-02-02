63a2e50f3bdca83d124dd3831e88be50edb6354f
0x1eef <0x1eef@hardenedbsd.org>
2026-02-01 22:07:58 -0300

    Update mdo.sh

--- libexec/sourcezap/functions/mdo.sh
+++ libexec/sourcezap/functions/mdo.sh
@@ -6,6 +6,28 @@ get_system_rules()
     sysctl -n security.mac.do.rules 2> /dev/null || true
 }
 
+get_sourcezap_rules()
+{
+    local libexec
+    libexec=$1
+    "${libexec}"/scripts/get-mdo-rule | tr ';' '\n' | sed '/^$/d'
+}
+
+is_rule_enabled()
+{
+    local system_rules rule_list syslist entry
+    system_rules=$1
+    rule_list=$2
+    syslist=$(printf "%s" "${system_rules}" | tr ';' '\n')
+    while IFS= read -r entry; do
+        [ -z "${entry}" ] && continue
+        printf "%s\n" "${syslist}" | grep -Fxq -- "${entry}" || return 1
+    done <<EOF
+${rule_list}
+EOF
+    return 0
+}
+
 merge_rules()
 {
     local lh rh
@@ -39,24 +61,3 @@ join_rules()
     printf "%s" "${rule_list}" | paste -sd ';' -
 }
 
-get_sourcezap_rules()
-{
-    local libexec
-    libexec=$1
-    "${libexec}"/scripts/get-mdo-rule | tr ';' '\n' | sed '/^$/d'
-}
-
-is_rule_enabled()
-{
-    local system_rules rule_list syslist entry
-    system_rules=$1
-    rule_list=$2
-    syslist=$(printf "%s" "${system_rules}" | tr ';' '\n')
-    while IFS= read -r entry; do
-        [ -z "${entry}" ] && continue
-        printf "%s\n" "${syslist}" | grep -Fxq -- "${entry}" || return 1
-    done <<EOF
-${rule_list}
-EOF
-    return 0
-}
